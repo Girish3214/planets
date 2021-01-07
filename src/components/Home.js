4e9a69ed9e4011d54f38276b1782793d1e37cc34
Girish3214 <71925088+Girish3214@users.noreply.github.com>
2021-01-07 16:15:04 +0530

    with background

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,18 +4,22 @@ import Planet from "../components/Planet";
 
 const Home = () => {
   const [data, setdata] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const DataView = async () => {
+      setLoading(true);
       const planetData = await axios.get(
         "https://assignment-machstatz.herokuapp.com/planet"
       );
       setdata(planetData.data);
     };
     DataView();
+    setLoading(false);
   }, []);
   return (
     <div>
+      {/* {loading ? "true" : console.log(loading)} */}
       <div className="container">
         {data.map((d) => (
           <Planet key={d.id} planet={d} />
